a3a8b377474ca9f9c2f9372394adde71b38abaae
Phalguna Kumar Rachoor <ethanhunt@Phalgunas-MacBook-Pro.local>
2021-03-15 12:35:57 +0530

    fixed the conflict

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -20,7 +20,6 @@ exports.createOne = (Subject) =>
           });
     };
 
-<<<<<<< HEAD
 // exports.getAll = (query) => (req,res,next)=>{
 //     authController.org.query({ query, oauth:JSON.parse(process.env.OAUTH) }, function(err, resp){
 //         if(!err && resp) {
@@ -39,20 +38,3 @@ exports.getData = async (query)=>{
   let result = await authController.org.query({ query, oauth:JSON.parse(process.env.OAUTH) });
   return result;
 }
-=======
-exports.getAll = (query) => (req,res,next)=>{
-    authController.org.query({ query, oauth:JSON.parse(process.env.OAUTH) }, function(err, resp){
-        if(!err) {
-          res.status(200).json({
-          status: 'success',
-          data:resp.records
-      });
-      }
-      else {
-        res.status(err.statusCode).json(err);
-        }
-      });
-      
-};
-
->>>>>>> c06424eff778f768e7a4bba22a0062819f0830d7
