66dfd68642abb0a9d81768adc19512895cf61425
Phalguna Kumar Rachoor <ethanhunt@Phalgunas-MacBook-Pro.local>
2021-03-19 11:33:47 +0530

    removed sessions required fields

--- controllers/validateController.js
+++ controllers/validateController.js
@@ -37,7 +37,7 @@ exports.JoiEventSchema = Joi.object({
       title__c:Joi.string().required(),
       startDate__c:Joi.date().required(),
       endDate__c:Joi.date().required(),
-      status__c:Joi.string().required().valid('Draft', 'Open', 'Sold Out', 'Closed'),
+      status__c:Joi.string().valid('Draft', 'Open', 'Sold Out', 'Closed'),
       registrationLimit__c:Joi.number().required(),
       startTime__c:Joi.string().required().regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/),
       endTime__c:Joi.string().required().regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/),
